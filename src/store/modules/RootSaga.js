43187ea86eb897e5e8613191758f2fe2dff0e6a6
Raiany <raianyrufino12@gmail.com>
2020-05-13 10:39:36 -0300

    config rootSaga

--- src/store/modules/RootSaga.js
+++ src/store/modules/RootSaga.js
@@ -1,7 +1,12 @@
 import {call,  all, put, takeLatest} from 'redux-saga/effects'
 
+/* retorna uma chamada para o all, neste all passamos uma array com
+   todos os nossos sagas. */
 export default function* rootSaga(){
     // return yeld all([
         
     // ]) 
 }
+
+/* O redux-saga funciona em cima do padrão generators, ele te permite esperar
+que uma parte do código termine de ser executada para então continuar a execução daquele método*/
