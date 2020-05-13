64d6dbe43c453b4d2a431837a3e2c36649d43250
Raiany <raianyrufino12@gmail.com>
2020-05-13 12:18:34 -0300

    RootReducer

---
+++ src/store/modules/RootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+/* retorna uma chamada para o all, neste all passamos uma array com
+   todos os nossos reducers. */
+
+export default combineReducers({
+    
+})
