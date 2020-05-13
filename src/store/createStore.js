88a10cfb28a0a3ded40f5dc162427dff14d2cc00
Raiany <raianyrufino12@gmail.com>
2020-05-13 10:10:00 -0300

    using redux-saga

---
+++ src/store/createStore.js
@@ -0,0 +1,3 @@
+import { createStore, applyMiddleware } from 'redux';
+
+export default (reducers, middlewares) => createStore(reducers, applyMiddleware(...middlewares));
---
+++ src/store/modules/RootSaga.js
@@ -0,0 +1,7 @@
+import {call,  all, put, takeLatest} from 'redux-saga/effecys'
+
+export default function* rootSaga(){
+    // return yeld all([
+        
+    // ]) 
+}
