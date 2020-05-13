631541873f5675c48c36131655dba8b396e3438a
Raiany <raianyrufino12@gmail.com>
2020-05-13 10:20:03 -0300

    how does redux-saga?

--- src/store/index.js
+++ src/store/index.js
@@ -17,3 +17,10 @@ const persistor = persistStore(store);
 sagaMiddleware.run(rootSaga);
 
 export { store, persistor };
+
+/* Como funciona o fluxo do redux-saga? 
+ -> O usuário clica em algum component, ele gera uma action no action create,
+ então será chamado uma action especial do redux-saga e ele vai tomar uma decisão do que deve
+ ser chamado e qual action deve ser disparada. E quando ele disparar a action, o reducer responsável
+ pela action vai atualizar o estado da store.
+*/
